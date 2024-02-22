aa6631dcd7cf3e9d8fd216914750266ca79b22eb
RAJI-Zakaria <zakariaraji.me@gmail.com>
2024-02-22 15:43:41 +0100

    fixing prisma-adapter configuration

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,17 @@
-import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import prisma from "@/prisma/client"
+import { PrismaAdapter } from "@auth/prisma-adapter"
 
 // i m willing to add adapter but it is not working yet.
-//TODO : there is a version error with the prisma adapter auth -- next-auth.
-//this part is not fully implemented yet.
-//----to be continued -----
+// DONE : by following the documentation, i have solved the problem.
+// there is a version error with the prisma adapter auth -- next-auth.
+// This part is not fully implemented yet.
+//---- Finished -----
 
 
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
     providers: [
         GoogleProvider({
           clientId: process.env.GOOGLE_CLIENT_ID!,
